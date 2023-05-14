3ce73ff425745c43651928f64a4f385a85125572
Misa898 <misa@newmanstech.com>
2023-05-14 12:11:26 +0300

    convert epoch time

--- ESP32_firebase.ino
+++ ESP32_firebase.ino
@@ -1,15 +1,3 @@
-
-/**
- * Created by K. Suwatchai (Mobizt)
- *
- * Email: k_suwatchai@hotmail.com
- *
- * Github: https://github.com/mobizt/Firebase-ESP32
- *
- * Copyright (c) 2023 mobizt
- *
- */
-
 #include <Arduino.h>
 #if defined(ESP32)
 #include <WiFi.h>
@@ -48,7 +36,7 @@
 
 #define DHTPIN 2
  
-// Definit le type de capteur utilise
+// Definit the sensor type being used
 #define DHTTYPE DHT11
  
 // Declare un objet de type DHT
@@ -68,10 +56,10 @@ String uid;
 
 // Database main path (to be updated in setup with the user UID)
 String databasePath;
+
 // Database child nodes
 String tempPath = "/temperature";
 String humPath = "/humidity";
-String presPath = "/pressure";
 String timePath = "/timestamp";
 
 // Parent Node (to be updated in every loop)
@@ -82,12 +70,43 @@ FirebaseJson json;
 
 const char* ntpServer = "pool.ntp.org";
 
-// BME280 sensor
-Adafruit_BME280 bme; // I2C
-float temperature;
-float humidity;
-
 unsigned long sendDataPrevMillis = 0;
+unsigned long timerDelay = 10000;
+
+// Initialize WiFi
+void initWiFi() {
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+  Serial.println();
+}
+
+// Uncomment this if want to to format the time
+/*
+
+void formatEpochTimeToString(time_t epochTime, char* str) {
+  struct tm timeinfo;
+  localtime_r(&epochTime, &timeinfo);
+  strftime(str, 20, "%d/%m/%Y:%H:%M", &timeinfo);
+}
+
+*/
+
+// Function that gets current epoch time
+unsigned long getTime() {
+  time_t now;
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    //Serial.println("Failed to obtain time");
+    return(0);
+  }
+  time(&now);
+  return now + 10800;
+}
 
 
 void setup()
@@ -95,59 +114,48 @@ void setup()
 
   Serial.begin(115200);
 
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(300);
-  }
-  Serial.println();
-  Serial.print("Connected with IP: ");
-  Serial.println(WiFi.localIP());
-  Serial.println();
+  initWiFi();
 
-  Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+  configTime(0, 0, ntpServer);
 
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  // Firebase.begin(&config, &auth);
+  config.api_key = API_KEY;
+
+  // Assign the user sign in credentials
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  // Assign the RTDB URL (required)
+  config.database_url = DATABASE_URL;
 
   // Comment or pass false value when WiFi reconnection will control by your code or third party library
   Firebase.reconnectWiFi(true);
 
   Firebase.setDoubleDigits(5);
 
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+
+  // Assign the maximum retry of token generation
+  config.max_token_generation_retry = 5;
+
+  Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+
+  Firebase.begin(&config, &auth);
+
+  // Getting the user UID might take a few seconds
+  Serial.println("Getting User UID");
+  while ((auth.token.uid) == "") {
+    Serial.print('.');
+    delay(1000);
+  }
+  // Print user UID
+  uid = auth.token.uid.c_str();
+  Serial.print("User UID: ");
+  Serial.println(uid);
+
+  // Update database path
+  databasePath = "/UsersData/" + uid + "/readings";
+
   dht.begin();
-
-  /** Timeout options.
-
-  //WiFi reconnect timeout (interval) in ms (10 sec - 5 min) when WiFi disconnected.
-  config.timeout.wifiReconnect = 10 * 1000;
-
-  //Socket connection and SSL handshake timeout in ms (1 sec - 1 min).
-  config.timeout.socketConnection = 10 * 1000;
-
-  //Server response read timeout in ms (1 sec - 1 min).
-  config.timeout.serverResponse = 10 * 1000;
-
-  //RTDB Stream keep-alive timeout in ms (20 sec - 2 min) when no server's keep-alive event data received.
-  config.timeout.rtdbKeepAlive = 45 * 1000;
-
-  //RTDB Stream reconnect timeout (interval) in ms (1 sec - 1 min) when RTDB Stream closed and want to resume.
-  config.timeout.rtdbStreamReconnect = 1 * 1000;
-
-  //RTDB Stream error notification timeout (interval) in ms (3 sec - 30 sec). It determines how often the readStream
-  //will return false (error) when it called repeatedly in loop.
-  config.timeout.rtdbStreamError = 3 * 1000;
-
-  Note:
-  The function that starting the new TCP session i.e. first time server connection or previous session was closed, the function won't exit until the
-  time of config.timeout.socketConnection.
-
-  You can also set the TCP data sending retry with
-  config.tcp_data_sending_retry = 1;
-
-  */
 }
 
 void loop()
@@ -155,81 +163,18 @@ void loop()
 
   // Firebase.ready() should be called repeatedly to handle authentication tasks.
 
-  if (Firebase.ready() && (millis() - sendDataPrevMillis > 2000 || sendDataPrevMillis == 0))
-  {
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > timerDelay || sendDataPrevMillis == 0)){
     sendDataPrevMillis = millis();
-    // String temperature = String(dht.readTemperature())+" °C";
-    // String humidity = String(dht.readHumidity())+" %";
-    Serial.println("Temperature = " + String(dht.readTemperature())+" °C");
-    Serial.println("Humidity = " + String(dht.readHumidity())+" %");
-    // For the usage of FirebaseJson, see examples/FirebaseJson/BasicUsage/Create_Edit_Parse.ino
-    FirebaseJson json;
 
-    json.set("temperature/" + String(count), String(dht.readTemperature())+" °C");
-    json.set("humidity/" + String(count), String(dht.readHumidity())+" %");
-    Serial.printf("Set sensor value... %s\n", Firebase.set(fbdo, F("/sensor"), json) ? "ok" : fbdo.errorReason().c_str());
+    //Get current timestamp
+    timestamp = getTime();
+    Serial.print ("time: ");
+    Serial.println (timestamp);
+    parentPath= databasePath + "/" + String(timestamp);
 
-    Serial.println();
-
-    // For generic set/get functions.
-
-    // For generic set, use Firebase.set(fbdo, <path>, <any variable or value>)
-
-    // For generic get, use Firebase.get(fbdo, <path>).
-    // And check its type with fbdo.dataType() or fbdo.dataTypeEnum() and
-    // cast the value from it e.g. fbdo.to<int>(), fbdo.to<std::string>().
-
-    // The function, fbdo.dataType() returns types String e.g. string, boolean,
-    // int, float, double, json, array, blob, file and null.
-
-    // The function, fbdo.dataTypeEnum() returns type enum (number) e.g. fb_esp_rtdb_data_type_null (1),
-    // fb_esp_rtdb_data_type_integer, fb_esp_rtdb_data_type_float, fb_esp_rtdb_data_type_double,
-    // fb_esp_rtdb_data_type_boolean, fb_esp_rtdb_data_type_string, fb_esp_rtdb_data_type_json,
-    // fb_esp_rtdb_data_type_array, fb_esp_rtdb_data_type_blob, and fb_esp_rtdb_data_type_file (10)
-
-    count++;
+    json.set(tempPath.c_str(), String(String(dht.readTemperature())+" °C"));
+    json.set(humPath.c_str(), String(String(dht.readHumidity())+" %"));
+    json.set(timePath, String(timestamp));
+    Serial.printf("Set json... %s\n", Firebase.RTDB.setJSON(&fbdo, parentPath.c_str(), &json) ? "ok" : fbdo.errorReason().c_str());
   }
 }
-
-/// PLEASE AVOID THIS ////
-
-// Please avoid the following inappropriate and inefficient use cases
-/**
- *
- * 1. Call get repeatedly inside the loop without the appropriate timing for execution provided e.g. millis() or conditional checking,
- * where delay should be avoided.
- *
- * Everytime get was called, the request header need to be sent to server which its size depends on the authentication method used,
- * and costs your data usage.
- *
- * Please use stream function instead for this use case.
- *
- * 2. Using the single FirebaseData object to call different type functions as above example without the appropriate
- * timing for execution provided in the loop i.e., repeatedly switching call between get and set functions.
- *
- * In addition to costs the data usage, the delay will be involved as the session needs to be closed and opened too often
- * due to the HTTP method (GET, PUT, POST, PATCH and DELETE) was changed in the incoming request.
- *
- *
- * Please reduce the use of swithing calls by store the multiple values to the JSON object and store it once on the database.
- *
- * Or calling continuously "set" or "setAsync" functions without "get" called in between, and calling get continuously without set
- * called in between.
- *
- * If you needed to call arbitrary "get" and "set" based on condition or event, use another FirebaseData object to avoid the session
- * closing and reopening.
- *
- * 3. Use of delay or hidden delay or blocking operation to wait for hardware ready in the third party sensor libraries, together with stream functions e.g. Firebase.RTDB.readStream and fbdo.streamAvailable in the loop.
- *
- * Please use non-blocking mode of sensor libraries (if available) or use millis instead of delay in your code.
- *
- * 4. Blocking the token generation process.
- *
- * Let the authentication token generation to run without blocking, the following code MUST BE AVOIDED.
- *
- * while (!Firebase.ready()) <---- Don't do this in while loop
- * {
- *     delay(1000);
- * }
- *
- */
